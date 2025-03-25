b900263da020260f2dee4973b6a9bde2b215c631
f0reachARR <f0reach@f0reach.me>
2025-03-25 17:09:05 +0900

    Add setup script

--- setup.bash
+++ setup.bash
@@ -5,7 +5,7 @@ set -e
 SCRIPT_DIR=$(readlink -f "$(dirname "$0")")
 AVAILABLE_PLAYBOOKS=$(find "$SCRIPT_DIR/playbooks" -maxdepth 1 -type f -name "*.yaml" -exec basename {} \; | sort)
 # List of default playbook
-DEFAULT_PLAYBOOKS="common.yaml"
+DEFAULT_PLAYBOOKS="00-common.yaml"
 
 echo -e "\e[36mInstalling dependencies...\e[m"
 
@@ -36,13 +36,13 @@ fi
 # Install pipx for ansible
 if ! (python3 -m pipx --version >/dev/null 2>&1); then
     sudo apt-get -y update
-    python3 -m pip install --user pipx
+    sudo apt-get install -y pipx
 fi
 
 # Install ansible
 python3 -m pipx ensurepath
 export PATH="${PIPX_BIN_DIR:=$HOME/.local/bin}:$PATH"
-# pipx install --include-deps --force "ansible==6.*"
+pipx install --include-deps --force "ansible==8.*"
 
 # Create map for default selected playbook
 declare -A PLAYBOOK_MAP
@@ -55,22 +55,23 @@ done
 
 # Collect playbook names with whiptail
 PLAYBOOKS=$(whiptail --title "Select Playbook" --checklist "Choose a playbook to run:" 15 60 6 \
-    $(for playbook in $AVAILABLE_PLAYBOOKS; do echo "$playbook" "$playbook" "${PLAYBOOK_MAP[$playbook]}"; done) 3>&1 1>&2 2>&3)
+    $(for playbook in $AVAILABLE_PLAYBOOKS; do echo $playbook $playbook "${PLAYBOOK_MAP[$playbook]}"; done) 3>&1 1>&2 2>&3)
 
 echo -e "\e[36mRunning playbook: $PLAYBOOKS\e[m"
 
 # Import env
 ansible_args=("--ask-become-pass")
 
+source amd64.env
 for env_name in $(sed -e "s/^\s*//" -e "/^#/d" -e "s/=.*//" <amd64.env); do
     ansible_args+=("--extra-vars" "${env_name}=${!env_name}")
 done
 
 for playbook in $PLAYBOOKS; do
-    ansible_args+=("$SCRIPT_DIR/playbooks/$playbook")
+    ansible_args+=("$SCRIPT_DIR/playbooks/${playbook//\"/}")
 done
 
 # Run ansible
-ansible-playbook -i "localhost," -c local "${ansible_args[@]}"
+ansible-playbook "${ansible_args[@]}"
 
 echo -e "\e[36mDone!\e[m"
